575e3dd3e3d6f6b79b1a34af0da3e23551c7424f
機関車ヘイヘイ\vantan <yu.love5416@gmail.com>
2022-10-08 13:05:52 +0900

    玉との衝突判定はつくった

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -18,4 +18,13 @@ public class BallController : MonoBehaviour
     {
         
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == _tagName)
+        {
+            Destroy(this.gameObject);
+        }
+        Debug.Log("a");
+    }
 }
