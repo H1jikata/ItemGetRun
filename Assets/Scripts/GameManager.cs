1592622ce81e90d3fbd7605be000a2956319b395
ひじかた <yu.love5416@gmail.com>
2022-10-19 19:57:10 +0900

    ScoreをテキストUIに反映させるようにした

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField,Tooltip("ScoreのUIについているTagの名前")] string _tagName = default;
+    [SerializeField, Tooltip("Scoreの前につける単語")] string _score = default;
+    int _currentScore = default;
+    bool _startTrigger = default;
+
+    GameObject _scoreObject;
+    Text _scoreText;
     void Start()
     {
-        
+        _currentScore = 0; //ゲームが始まったら必ず初期化する
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_startTrigger)　//ゲームスタート後にスコアを反映させる
+        {
+            _scoreObject = GameObject.FindWithTag(_tagName); //Findの処理が重いことを知っているが、このゲームはそこまで重くならないので楽なFindを採用
+            _scoreText = _scoreObject.GetComponent<Text>();
+            _scoreText.text = _score + _currentScore;
+        }
+    }
+
+    /// <summary>
+    /// デバッグ用
+    /// </summary>
+    public void OnButtomDown()
+    {
+        _startTrigger = true;
     }
 }
