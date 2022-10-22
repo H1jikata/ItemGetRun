66ff7e15802987030db4ea695a04b6aeee061bed
機関車ヘイヘイ\vantan <yu.love5416@gmail.com>
2022-10-22 14:12:14 +0900

    スロットの部分的追加

---
+++ Assets/Scripts/slotte.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slotte : MonoBehaviour
+{
+    [SerializeField,Tooltip("乱数の一番したと一番うえ")] float _min, _max;
+    [SerializeField, Tooltip("スロットのあたり確率")] float _pMin, _pMax;
+    [SerializeField, Tooltip("スロットの数字オブジェクト")] GameObject[] _number = default;
+
+    int _luccky = default;
+    float _unLuccky = default;
+    int[] _avoidNum = default; 
+    void Start()
+    {
+        _avoidNum = new int[3];
+        _luccky = (int)Random.Range(_pMin, _pMax);
+
+        for(int i = 0; i < _avoidNum.Length; i++) //　一度一度数字をとめる
+        {
+            _unLuccky = Random.Range(_min, _max);
+            _avoidNum[i] = (int)_unLuccky;
+            Debug.Log(_avoidNum[i]);
+        }
+    }
+}
