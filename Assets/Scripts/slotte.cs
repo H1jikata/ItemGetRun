6f4e8d92602dc9ffbb30a421546f8401658794c9
ひじかた <yu.love5416@gmail.com>
2022-11-19 23:55:19 +0900

    enumの追加

--- Assets/Scripts/slotte.cs
+++ Assets/Scripts/slotte.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class slotte : MonoBehaviour
 {
-    [SerializeField,Tooltip("乱数の一番したと一番うえ")] float _min, _max;
+    [SerializeField, Tooltip("乱数の一番したと一番うえ")] float _min, _max;
     [SerializeField, Tooltip("スロットのあたり確率")] float _pMin, _pMax;
     [SerializeField] int _hitNum = default;
     [SerializeField] GameObject _reel = default;
@@ -14,16 +14,16 @@ public class slotte : MonoBehaviour
 
     int _luccky = default;
     float _unLuccky = default;
-    int[] _avoidNum = default; 
+    int[] _avoidNum = default;
     void Start()
     {
         _avoidNum = new int[3];
         _luccky = (int)Random.Range(_pMin, _pMax);
 
-        if(_hitNum == _luccky)
+        if (_hitNum == _luccky)
         {
             int _num = (int)Random.Range(1, 9);
-            for(int i = 0; i < 3; i++) // もし当たったらランダムで数字を出すように設定
+            for (int i = 0; i < 3; i++) // もし当たったらランダムで数字を出すように設定
             {
                 Instantiate(_number[_num - 1], _posi[i].transform);
             }
@@ -43,4 +43,18 @@ public class slotte : MonoBehaviour
     {
         Vector3 _currentePos = this._reel.transform.position; // 初期位置を保存
     }
+
+    public enum SlotteNum
+    {
+        one,
+        two,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        nine
+    }
+
 }
